4cbaaf2d67756c29f95470ebc703f538f75c4c86
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-07-20 21:50:37 +0530

    .env file added in gitignore

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,47 +4,58 @@ import axios from 'axios'
 import Articles from '../../components/Articles/Articles'
 import toast from 'react-hot-toast'
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("india")
     const [news, setNews] = useState([])
 
     const loadNews = async () => {
         toast.loading("Loading Latest News")
-        const response = await axios.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6e2a48d5e4be43548704866feaf1116f")
-        setNews(response.data.articles)
+        toast.dismiss()
+        try {
+            const response = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-06-20&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`)
+            setNews(response.data.articles)
+            setTimeout(() => {
+                toast.success("News Loaded Successfully")
+            }, 1000)
+        }
+        catch (error) {
+            toast.error("Failed to Load News. Please Try Again")
+        }
 
-        
-        setTimeout(() => {
-            toast.dismiss()
-            toast.success("News Loaded Successfully")
-        }, 1000)
     }
-    
+
     useEffect(() => {
         setTimeout(() => {
-        loadNews() 
-    }, 1000)
+            loadNews()
+        }, 1000)
     }, [])
 
+    useEffect(() => {
+        setTimeout(() => {
+            loadNews()
+        }, 1000)
+    }, [searchQuery])
 
 
     return (
-        <div className='container'>
-            <div className='row justify-content-center'>
-                {news.map((newsArticles, i) => {
-                    const { author, description, url, content, title, urlToImage, publishedAt } = newsArticles
-                    return (
-                        <>
-                        <div className='col-xl-8 col-lg-6 col-sm-12'>
-                            <Articles key={i} author={author}
-                                description={description} url={url} content={content} title={title} urlToImage={urlToImage} publishedAt={publishedAt}
-                            />
-                        </div>
-                        <hr/>
-                        </>
-                    )
-                })}
+        <>
+            <div className='container mt-5'>
+                <div className='row justify-content-center'>
+                    {news.map((newsArticles, i) => {
+                        const { author, description, url, content, title, urlToImage, publishedAt } = newsArticles
+                        return (
+                            <>
+                                <div className='col-xl-8 col-lg-6 col-sm-12'>
+                                    <Articles key={i} author={author}
+                                        description={description} url={url} content={content} title={title} urlToImage={urlToImage} publishedAt={publishedAt}
+                                    />
+                                </div>
+                                <hr />
+                            </>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-
+        </>
     )
 }
 
